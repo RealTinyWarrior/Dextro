3cc862341f5d8c194439df9eea880909da08ee0a
RealTinyWarrior <sincpls@gmail.com>
2023-09-01 18:20:37 +0600

    Fixed npm start

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ client.on("ready", () => {
 client.on("messageCreate", (message) => {
     if (message.author.bot || !message.content.startsWith(prefix)) return;
 
-    const cmd = message.content.slice(2).split(" ");
+    const cmd = message.content.slice(prefix.length).split(" ");
+    const send = (m) => message.channel.send(m);
 });
 
 client.login(process.env.TOKEN);
